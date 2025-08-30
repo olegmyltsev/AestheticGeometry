baee33042d1e81c01bed68e9e9a8e4d9901f9d1a
Oleg <omyltsev@gmail.com>
2025-08-30 17:07:52 +0300

    before refactoring 0

--- src/js/dev.js
+++ src/js/dev.js
@@ -1,17 +1,31 @@
-input = document.getElementById('file1')
+import { parseDdu } from "./dodeca-parser.js";
+import {dodecaView} from "./dodeca-view.js"
 
-readF = () => {
-    let file = new File([Text], '../Peacock.ddu')
+let btn = document.getElementById('btn')
+let input = document.getElementById('file1')
 
+
+btn.addEventListener('click',  () => {
+   readFile(input.files[0])    
+});
+
+
+function readFile(file /*file from input*/) {
+
+    let result
     let reader = new FileReader();
 
     reader.readAsText(file);
 
     reader.onload = function () {
-        console.log(reader.result);
+        console.log(parseDdu(reader.result));
+        
+        dodecaView(parseDdu(reader.result))
     };
 
     reader.onerror = function () {
-        console.log(reader.error);
+        result = reader.error
     };
+
 }
+
