e9de3860d170d74d235663e6ca06e302e0ce02a3
Oleg <omyltsev@gmail.com>
2026-01-31 02:01:35 +0300

    url fix and build

--- src/components/sections/DduGallery/DduGalleryCard/DduGalleryCard.jsx
+++ src/components/sections/DduGallery/DduGalleryCard/DduGalleryCard.jsx
@@ -11,12 +11,14 @@ const DduGalleryCard = ({ name, path = '' }) => {
         getDdu(path).then((data) => updateFile(name, data))
 
     }
+    console.log('https://raw.githubusercontent.com/olegmyltsev/AestheticGeometry/dev/src/DduStorage/dduImg/' + encodeURI(name) + '.png');
+    
 
     return (
         <div className='DduGalleryCard' onClick={cardClickHandle}>
 
             <div className="cardImg" style={{
-                backgroundImage: 'url(https://raw.githubusercontent.com/olegmyltsev/AestheticGeometry/21e8040e28df70efa5666a55ebcc8b813972ee0b/src/DduStorage/dduImg/' + encodeURI(name) + '.png)'
+                backgroundImage: 'url(https://raw.githubusercontent.com/olegmyltsev/AestheticGeometry/dev/src/DduStorage/dduImg/' + encodeURI(name) + '.png)'
             }}>
             </div>
             <div className="cardHeader">{name}</div>
